2629a722ec5e853e98d9149ba63e79c0583d4669
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-02-24 12:45:28 +0800

    add parts
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- src/components/chi-header/index.js
+++ src/components/chi-header/index.js
@@ -2,15 +2,29 @@
 import { Element } from '@polymer/polymer/polymer-element';
 import { ChiScheduleMixin } from 'chi-schedule-mixin';
 import { customElements } from 'global/window';
+import { LittleQStoreMixin } from '@littleq/state-manager';
 import '@polymer/polymer/lib/elements/dom-repeat';
 
 // define style and template
 import style from './style.styl';
 import template from './template.html';
 
-class Component extends ChiScheduleMixin(Element) {
+class Component extends LittleQStoreMixin(ChiScheduleMixin(Element)) {
   static get is () { return 'chi-header'; }
   static get template () { return `<style>${style}</style>${template}`; }
+
+  static get properties () {
+    return {
+      params: {
+        type: Object,
+        statePath: 'littleqSmallRouter.params'
+      },
+      route: {
+        type: String,
+        statePath: 'littleqSmallRouter.route'
+      }
+    };
+  }
 
   openNavigation () {
     this.shadowRoot.querySelector('.menu').classList.add('hidden');
@@ -23,6 +37,12 @@ class Component extends ChiScheduleMixin(Element) {
     this.shadowRoot.querySelector('.close').classList.add('hidden');
     this.shadowRoot.querySelector('.phone').classList.remove('open');
   }
+
+  _isSelected (paramsDayId, dayId) { return paramsDayId === dayId ? 'selected' : ''; }
+
+  _isScheduleAtAGlance (route) { return route === '/' ? 'selected' : ''; }
+
+  _isSearch (route) { return route === '/search' ? 'selected' : ''; }
 }
 
 !customElements.get(Component.is)
